699c9f1f25d335d6e201e6082f2858f449352632
Anh Từ <44427982+anhtu163@users.noreply.github.com>
2019-11-30 00:22:04 +0700

    Cài bootstrap, Thêm các component CardTutor, HomePage, Menu

---
+++ src/components/HomePage.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import Menu from './Menu';
+import CardTutor from './CardTutor';
+
+import './App.css';
+
+function HomePage() {
+  return (
+    <div className="bg-light">
+      <Menu />
+      <div className="row ">
+      <div className="col-md-3 ">
+          <div className="row row-cols-1 row-cols-md-2">
+            <p className=" col m-md-2 ml-md-4 mytitle separate ">PHÂN LOẠI</p>
+          </div>
+          <div className = "myscroll">
+            <div className="accordion" id="accordionExample">
+              <div className="card mr-md-2 ml-md-2">
+                <div className="card-header" id="headingOne">
+                  <h2 className="mb-0">
+                    <button
+                      className="btn"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Môn học
+                    </button>
+                  </h2>
+                </div>
+
+                <div
+                  id="collapseOne"
+                  className="collapse show"
+                  aria-labelledby="headingOne"
+                  data-parent="#accordionExample"
+                >
+                  <div className="list-group">
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Ngữ Văn
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Toán Học
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Vật Lý
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Sinh Học
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Hóa Học
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lịch Sử
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Địa Lý
+                    </a>
+
+                    
+                  </div>
+                </div>
+              </div>
+              <div className="card mr-md-2 ml-md-2 ">
+                <div className="card-header" id="headingTwo">
+                  <h2 className="mb-0">
+                    <button
+                      className="btn collapsed"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      Khối
+                    </button>
+                  </h2>
+                </div>
+                <div
+                  id="collapseTwo"
+                  className="collapse"
+                  aria-labelledby="headingTwo"
+                  data-parent="#accordionExample"
+                >
+                   <div className="list-group">
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 1
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 2
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 3
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 4
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 5
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 6
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 7
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 8
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 9
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 10
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 11
+                    </a>
+                    <a href="/" className="list-group-item list-group-item-action">
+                      Lớp 12
+                    </a>
+
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-9 ">
+          <div className="row row-cols-1 row-cols-md-1">
+            <p className=" col m-md-2 mr-md-4 mytitle separate">
+              DANH SÁCH GIA SƯ
+            </p>
+          </div>
+          <div className="row row-cols-1 row-cols-md-1 myscroll">
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+            <div className="col mb-3">
+              <CardTutor />
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/components/Menu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+function Menu() {
+  return (
+   
+    <nav className="navbar navbar-expand-sm navbar-dark mybg m-md-2 myshadow ">
+      <a className="navbar-brand" href="/">UBER FOR TUTOR</a>
+      <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+          aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"/>
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavId">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Trang Chủ <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Link</a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+        
+          <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+          <button className="btn btn-outline-light my-2 mr-sm-2" type="submit">Tìm Gia Sư</button>
+        <button className="btn btn-outline-light my-2 mr-sm-2" type="submit">Tìm Công Việc</button>
+        </form>
+      </div>
+    </nav>
+    
+    
+  );
+}
+
+export default Menu;
