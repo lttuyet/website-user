d43e31a963f098b65097b162ac692f1e06ed8e7a
lttuyet <44430765+lttuyet@users.noreply.github.com>
2019-12-20 20:10:00 +0700

    Update register - devide address

--- src/components/Register.js
+++ src/components/Register.js
@@ -29,7 +29,40 @@ class Register extends PureComponent {
       address: '',
       email: '',
       password: '',
-      errorInfo: ''
+      errorInfo: '',
+      addresses: [
+        {
+          province: "Thành phố Hồ Chí Minh",
+          districts: [
+            "Quận 1",
+            "Quận 2",
+            "Quận 3",
+            "Quận 4",
+            "Quận 5",
+            "Quận 6",
+            "Quận 7",
+            "Quận 8",
+            "Quận 9",
+            "Quận 10",
+            "Quận 11",
+            "Quận 12",
+            "Quận Bình Tân",
+            "Quận Bình Thạnh",
+            "Quận Gò Vấp",
+            "Quận Phú Nhuận",
+            "Quận Tân Bình",
+            "Quận Tân Phú",
+            "Quận Thủ Đức",
+            "Huyện Bình Chánh",
+            "Huyện Cần Giờ",
+            "Huyện Củ Chi",
+            "Huyện Hóc Môn",
+            "Huyện Nhà Bè",
+          ]
+        }
+      ],
+      province: "Thành phố Hồ Chí Minh",
+      district: "Quận 1",
     };
   }
 
@@ -53,8 +86,20 @@ class Register extends PureComponent {
     this.setState({ password: e.target.value });
   }
 
+  handleProvinceChange(e) {
+    const st=this.state;
+    const address = st.addresses.find(item => item.province === e.target.value);
+    
+    this.setState({ province: e.target.value ,district:address.districts[0]});
+  }
+
+  handleDistrictChange(e) {
+    this.setState({ district: e.target.value });
+  }
+
   render() {
     const st = this.props;
+    const state = this.state;
 
     if (st.isLogin) {
       return <Redirect to="/" />;
@@ -72,7 +117,7 @@ class Register extends PureComponent {
 
       const response = callAPI('user/register', 'POST', user).then((r) => {
         try {
-          const {status} = r.data;
+          const { status } = r.data;
 
           if (status === 500) {
             this.setState({ errorInfo: 'Tài khoản đã tồn tại!' });
@@ -99,7 +144,7 @@ class Register extends PureComponent {
 
       const response = callAPI('user/register', 'POST', user).then((res) => {
         try {
-          const {status} = res.data;
+          const { status } = res.data;
 
           if (status === 500) {
             this.setState({ errorInfo: 'Tài khoản đã tồn tại!' });
@@ -114,7 +159,19 @@ class Register extends PureComponent {
       });
     };
 
-    
+    const listProvince = state.addresses.map((address) => {
+      return (
+        <option value={address.province}>{address.province}</option>
+      );
+    });
+
+    const address = state.addresses.find(item => item.province === state.province);
+    const listDisTrict = address.districts.map((district) => {
+      return (
+        <option value={district}>{district}</option>
+      );
+    });
+
     return (
       <div className="container-fluid">
         <Menu />
@@ -122,10 +179,11 @@ class Register extends PureComponent {
           <form className="form-signin myshadow"
             onSubmit={(e) => {
               e.preventDefault();
+              const address=`${this.state.address}, ${this.state.district}, ${this.state.province}`;
 
               const user = {
                 name: this.state.name,
-                address: this.state.address,
+                address,
                 role: this.state.role,
                 email: this.state.email,
                 password: this.state.password,
@@ -133,7 +191,7 @@ class Register extends PureComponent {
               };
               const res = callAPI('user/register', 'POST', user).then((res) => {
                 try {
-                  const {status} = res.data;
+                  const { status } = res.data;
 
                   if (status === 500) {
                     this.setState({ errorInfo: 'Tài khoản đã tồn tại!' });
@@ -150,7 +208,7 @@ class Register extends PureComponent {
               <h1 className="h3 font-weight-normal separate">Đăng ký</h1>
             </div>
             <div className="form-label-group ">
-              <div><label className=" col-form-label-sm ml-md-1">Loại tài khoản:</label></div>
+              <div><label className=" ml-md-1">Loại tài khoản:</label></div>
               <select className="custom-select custom-select form-control" value={this.state.role} onChange={this.handleRoleChange}>
                 <option value="learner" >Người học</option>
                 <option value="tutor">Người dạy</option>
@@ -168,6 +226,22 @@ class Register extends PureComponent {
               />
               <label htmlFor="inputName">Họ tên</label>
             </div>
+            <div className="form-label-group ">
+              <div className="row  mx-auto d-block">
+                <div className="ml-md-1">
+                  <label>Tỉnh/Thành phố</label>
+                  <select className="form-control" onChange={this.handleProvinceChange}>
+                    {listProvince}
+                  </select>
+                </div>
+                <div className="ml-md-1">
+                  <label>Quận/Huyện</label>
+                  <select className="form-control" onChange={this.handleDistrictChange}>
+                    {listDisTrict}
+                  </select>
+                </div>
+              </div>
+            </div>
             <div className="form-label-group">
               <input
                 type="text"
@@ -176,7 +250,7 @@ class Register extends PureComponent {
                 required
                 value={this.state.address} onChange={this.handleAddressChange}
               />
-              <label htmlFor="inputAddress">Địa chỉ</label>
+              <label htmlFor="inputAddress">Số nhà, Đường</label>
             </div>
             <div className="form-label-group">
               <input
@@ -229,7 +303,7 @@ class Register extends PureComponent {
             </div>
           </form>
         </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
