82776e8c7be553c909acf34d6cfd2dfb7cf82835
lttuyet <44430765+lttuyet@users.noreply.github.com>
2019-12-21 15:32:15 +0700

    finished detailed tutor

--- src/components/DetailsTutor/index.js
+++ src/components/DetailsTutor/index.js
@@ -57,7 +57,7 @@ class DetailsTutor extends PureComponent {
             });
         } catch (e) {
             this.setState({
-                error: 2
+                error: "Lỗi kết nối! Vui lòng load lại trang!"
             });
         }
     };
@@ -70,79 +70,104 @@ class DetailsTutor extends PureComponent {
 
     render() {
         const st = this.state;
-        let { error } = st;
+        const { error } = st;
+
+        if (error !== 0) {
+            return (
+                <div className="bg-light container-fluid">
+                    <Menu />
+                    <div>
+                        <nav aria-label="breadcrumb ">
+                            <ol className="breadcrumb bg-white ">
+                                <li className="breadcrumb-item text-info">
+                                    <a href="/">Trang chủ</a>
+                                </li>
+                                <li className="breadcrumb-item text-info">
+                                    <a href="/listtutors">Danh sách gia sư</a>
+                                </li>
+                                <li className="breadcrumb-item active" aria-current="page">
+                                    {st.tutor.name}
+                                </li>
+                            </ol>
+                        </nav>
+                        <div className="alert alert-danger alert-dismissible fade show mb-0" role="alert" >
+                            <i className="fa fa-check mx-2" />
+                            <strong>Thất bại!</strong>
+                            {" "}{error}
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            );
+        }
 
         // Danh sách hợp đồng
         const listContracts = st.contracts.filter(item => {
-            if (item.status === 'Bị khiếu nại'||item.status==='Đã thanh toán') {
+            if (item.status === 'Bị khiếu nại' || item.status === 'Đã thanh toán') {
                 return true;
             }
 
             return false;
         });
-        
-        const listShowContracts = listContracts.map((contract) => {
+
+        const size = Math.ceil(listContracts.length / 6);
+        const { page } = this.state;
+        const start = page * 6;
+        const listCurrentContracts = listContracts.slice(start, start + 6);
+
+        let listShowContracts = listCurrentContracts.map((contract) => {
             return (
-                <HistoryContract value={contract}/>
+                <HistoryContract value={contract} />
             );
         });
+
+        if (listShowContracts.length === 0) {
+            listShowContracts = (<h2 className="text-secondary">Chưa có hợp đồng</h2>);
+        }
 
         return (
             <div className="bg-light container-fluid">
                 <Menu />
-                {st.error !== 0 ? (
-                    <div
-                        className="alert alert-danger alert-dismissible fade show mb-0"
-                        role="alert"
-                    >
-                        <button
-                            type="button"
-                            className="close"
-                            data-dismiss="alert"
-                            aria-label="Close"
-                        >
-                            <span aria-hidden="true">×</span>
-                        </button>
-                        <i className="fa fa-check mx-2" />
-                        <strong>Thất bại!</strong>
-                        {error}
-                    </div>
-                ) : (
-                        <div>
-                            <nav aria-label="breadcrumb ">
-                                <ol className="breadcrumb bg-white ">
-                                    <li className="breadcrumb-item text-info">
-                                        <a href="/">Trang chủ</a>
-                                    </li>
-                                    <li className="breadcrumb-item text-info">
-                                        <a href="/listtutors">Danh sách gia sư</a>
-                                    </li>
-                                    <li className="breadcrumb-item active" aria-current="page">
-                                        {st.tutor.name}
-                                    </li>
-                                </ol>
-                            </nav>
-                            <div className="row">
-                                <div className="container col-4">
-                                    <div className="card mt-md-1">
-                                        <div className="container card-body">
-                                            <div className="wrapper ">
-                                                <InfoTutor tutor={st.tutor} contracts={st.contracts} tags={st.tags} />
-                                            </div>
+                <div>
+                    <nav aria-label="breadcrumb ">
+                        <ol className="breadcrumb bg-white ">
+                            <li className="breadcrumb-item text-info">
+                                <a href="/">Trang chủ</a>
+                            </li>
+                            <li className="breadcrumb-item text-info">
+                                <a href="/listtutors">Danh sách gia sư</a>
+                            </li>
+                            <li className="breadcrumb-item active" aria-current="page">
+                                {st.tutor.name}
+                            </li>
+                        </ol>
+                    </nav>
+                    {st.loaded &&
+                        <div className="row">
+                            <div className="container col-4">
+                                <div className="card mt-md-1">
+                                    <div className="container card-body">
+                                        <div className="wrapper ">
+                                            <InfoTutor tutor={st.tutor} contracts={st.contracts} tags={st.tags} />
                                         </div>
                                     </div>
                                 </div>
-                                <div className="container col-8">
-                                    <div className="row m-md-2 mb-md-1">
-                                        <h4 className="separate text-center text-danger">LỊCH SỬ DẠY HỌC</h4>
-                                    </div>
-                                    <div className="myscroll">
-                                        {listShowContracts}
-                                    </div>
+                            </div>
+                            <div className="container col-8">
+                                <div className="row m-md-2 mb-md-1">
+                                    <h4 className="separate text-center text-danger">LỊCH SỬ DẠY HỌC</h4>
+                                </div>
+                                <div className="myscroll">
+                                    {listShowContracts}
+                                </div>
+                                <div className="row mt-md-1 mr-md-2 float-right">
+                                    <ListPages className="page" current={page} size={size} onClick={(p) => { this.setPage(p); }} />
                                 </div>
                             </div>
                         </div>
-                    )}
+                    }
+                </div>
+                <Footer />
             </div>
         );
     }
