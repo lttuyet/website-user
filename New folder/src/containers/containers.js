f3b4e7d3175c9af9497eebf530d2afd3cd3c236d
lttuyet <44430765+lttuyet@users.noreply.github.com>
2019-12-26 20:47:46 +0700

    up host

--- New folder/src/containers/containers.js
+++
@@ -1,6 +0,0 @@
-  
-import Root from '../components/Root';
-
-const containers = Root;
-
-export default containers;
--- New folder/src/reducers/myReducer.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import AccountReducer from './AccountReducer';
-
-export default AccountReducer;
